617e0098d4b6f4a6b46a2bbcb8c4637a87705c47
Aaron Saray <me@aaronsaray.com>
2023-11-30 14:10:36 -0600

    Progress.

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,17 +9,124 @@ export default {
       blue: {
         1: '#0071BC',
         2: '#29abe2',
-        3: '#a5f3fc',
+        3: '#B3C4D9',
+        201: '#0071BC33',
+        202: '#29abe233',
       },
-      'light-blue': '#B3C4D9',
       'dark-blue': {
         1: '#0F1B2B',
         2: '#0E2439',
       },
-      'gradient-blue': {
-        start: '#29aBe2',
-        end: '#0071bc',
+      'dark-green': {
+        1: '#143E44',
       },
+      green: {
+        1: '#5FDDB7',
+        2: '#72F8CF',
+      },
+    },
+    fontFamily: {
+      sans: ['Space Grotesk', 'sans-serif'],
+    },
+    fontSize: {
+      'h1-b': [
+        '52px',
+        {
+          lineHeight: '64px',
+          fontWeight: '700',
+        },
+      ],
+      'h1-l': [
+        '52px',
+        {
+          lineHeight: '64px',
+          fontWeight: '300',
+        },
+      ],
+      'h2-b': [
+        '40px',
+        {
+          lineHeight: '47px',
+          fontWeight: '700',
+        },
+      ],
+      'h2-l': [
+        '40px',
+        {
+          lineHeight: '47px',
+          fontWeight: '300',
+        },
+      ],
+      'h3-b': [
+        '31px',
+        {
+          lineHeight: '37px',
+          fontWeight: '700',
+        },
+      ],
+      'h3-l': [
+        '31px',
+        {
+          lineHeight: '37px',
+          fontWeight: '300',
+        },
+      ],
+      'h4-b': [
+        '22px',
+        {
+          lineHeight: '27px',
+          fontWeight: '700',
+        },
+      ],
+      'h4-l': [
+        '22px',
+        {
+          lineHeight: '27px',
+          fontWeight: '300',
+        },
+      ],
+      'h5-b': [
+        '18px',
+        {
+          lineHeight: '27px',
+          fontWeight: '700',
+        },
+      ],
+      'h5-l': [
+        '18px',
+        {
+          lineHeight: '27px',
+          fontWeight: '300',
+        },
+      ],
+      sm: [
+        '14px',
+        {
+          lineHeight: '23px',
+          fontWeight: '700',
+        },
+      ],
+      md: [
+        '18px',
+        {
+          lineHeight: '27px',
+          fontWeight: '300',
+        },
+      ],
+      base: [
+        '18px',
+        {
+          lineHeight: '27px',
+          fontWeight: '300',
+        },
+      ],
+      lg: [
+        '22px',
+        {
+          lineHeight: '31px',
+          fontWeight: '300',
+        },
+      ],
     },
     extend: {},
     plugins: [],
