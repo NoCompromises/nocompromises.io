bcb0f10bf94ee92b830dd5aca6ff83aaf3afa80d
Aaron Saray <me@aaronsaray.com>
2023-11-30 09:18:57 -0600

    working on items

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,25 @@ export default {
     "./index.html"
   ],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'blue': {
+        1: '#0071BC',
+        2: '#a5f3fc',
+      },
+      'light-blue': '#B3C4D9',
+      'dark-blue': {
+        1: "#0F1B2B",
+        2: "#0E2439"
+      },
+      'gradient-blue': {
+        'start': '#29aBe2',
+        'end': "#0071bc"
+      }
+    },
     extend: {},
-  },
-  plugins: [],
+    plugins: [],
+  }
 }
-
