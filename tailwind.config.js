45ade4da13974a8cb414629be72967f883d82172
Aaron Saray <me@aaronsaray.com>
2023-11-30 10:07:27 -0600

    added eslint properly.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html"
+    './index.html',
   ],
   theme: {
     colors: {
       transparent: 'transparent',
       current: 'currentColor',
-      'white': '#ffffff',
-      'blue': {
+      white: '#ffffff',
+      blue: {
         1: '#0071BC',
         2: '#a5f3fc',
       },
       'light-blue': '#B3C4D9',
       'dark-blue': {
-        1: "#0F1B2B",
-        2: "#0E2439"
+        1: '#0F1B2B',
+        2: '#0E2439',
       },
       'gradient-blue': {
-        'start': '#29aBe2',
-        'end': "#0071bc"
-      }
+        start: '#29aBe2',
+        end: '#0071bc',
+      },
     },
     extend: {},
     plugins: [],
-  }
-}
+  },
+};
